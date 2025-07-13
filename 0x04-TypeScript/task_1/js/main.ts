e12544b5ea0b9ee623e54c04f8a3121f25687190
devhan-hub <hanabduman@gmail.com>
2025-07-13 22:27:59 +0300

    finalizing task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// 
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string; 
+    [propName:string]: any;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName:string , lastName:string):string;
+}
+
+const printTeacher:printTeacherFunction = (firstName , lastName) => {
+     return firstName.charAt(0)+ "." + lastName;
+}
